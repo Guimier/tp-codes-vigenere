19d8b46a91e4422881af68bdd991fbf182e03331
Lucien Guimier <lucien.guimier@laposte.net>
2015-11-30 10:41:13 +0100

    Chiffrage/déchiffrage fonctionnel

--- main_cipher.cpp
+++ main_cipher.cpp
@@ -2,51 +2,55 @@
 #include <string>
 using namespace std;
 
-class Vigenere
+struct Vigenere
 {
-public:
-  string key;
- 
-  Vigenere(string key)
-  {
-    // Modifying all characters other than uppercase : lowercase -> uppercase, other -> delete
-    for(unsigned int i = 0; i < key.size(); ++i)
-    {
-      if(key[i] >= 'A' && key[i] <= 'Z')
-        this->key += key[i];
-      else if(key[i] >= 'a' && key[i] <= 'z')
-        this->key += key[i] + 'A' - 'a';
-    }
-  }
- 
-  string encrypt(string text)
-  {
-    string out;
+	string key;
 
-    // Modifying all characters other than uppercase : lowercase -> uppercase, other -> delete
-    for(unsigned int i = 0; i < text.length(); ++i)
-    {
-      if(text[i] >= 'A' && text[i] <= 'Z')
-        out += text[i];
-      else if(text[i] >= 'a' && text[i] <= 'z')
-        out += text[i] + 'A' - 'a';
-    }
+	Vigenere(string key)
+	{
+		// Modifying all characters other than uppercase : lowercase -> uppercase, other -> delete
+		for(unsigned int i = 0; i < key.size(); ++i)
+		{
+			if(key[i] >= 'A' && key[i] <= 'Z')
+				this->key += key[i] - 'A';
+			else if(key[i] >= 'a' && key[i] <= 'z')
+				this->key += key[i] - 'a';
+		}
+	}
 
-    // ADD THE VIGENERE CRYPTION 	
- 
-    return out;
-  }
- 
-  string decrypt(string text)
-  {
-    string out;
+	string encrypt(string text)
+	{
+		string out;
 
-    // TO REMOVE
-    out = text;
+		// Modifying all characters other than uppercase : lowercase -> uppercase, other -> delete
+		for(unsigned int i = 0; i < text.length(); ++i)
+		{
+			if(text[i] >= 'A' && text[i] <= 'Z')
+				out += text[i];
+			else if(text[i] >= 'a' && text[i] <= 'z')
+				out += text[i] + 'A' - 'a';
+		}
 
-    // ADD THE VIGENERE DECRYPTION 
-    return out;
-  }
+		// ADD THE VIGENERE CRYPTION 	
+		for (size_t i = 0 ; i <out.size() ; ++i)
+		{
+			out[i] = ( ( out[i] - 'A' + key[i%key.size()] ) % 26 ) + 'A';
+		}
+
+		return out;
+	}
+
+	string decrypt(string text)
+	{
+		string out;
+
+		for (size_t i = 0 ; i <text.size() ; ++i)
+		{
+			out += ( ( text[i] - 'A' - key[i%key.size()] + 26 ) % 26 ) + 'A';
+		}
+
+		return out;
+	}
 };
 
 //////////////////////////////////////////////////////////////////
